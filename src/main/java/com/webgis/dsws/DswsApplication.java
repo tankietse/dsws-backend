272565730116e35c8f92876935f2000eb303b226
tankietse <kiettuanleo@gmail.com>
2024-11-07 10:36:07 +0700

    Init backend

---
+++ src/main/java/com/webgis/dsws/DswsApplication.java
@@ -0,0 +1,13 @@
+package com.webgis.dsws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webgis/dsws/DswsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webgis.dsws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
